b92906d144a3524ae836c8447c07790f2ac4c7c1
Jwhitby <jw89@icloud.com>
2021-06-01 19:06:21 +0100

    moving ports

--- ship.js
+++ ship.js
@@ -1,5 +1,12 @@
 // As a cruise ship captain,
-// So I can get passengers aboard a ship,
-// I want a ship to have a starting port.
-function Ship(port) {}
+// So I can get passengers started on their journey,
+// I want a ship to be able to set sail from a port.
+function Ship(port) {
+  this.currentPort = port;
+
+  this.setSail = () => {
+    this.currentPort = "";
+  };
+}
+
 module.exports = Ship;
--- ship.test.js
+++ ship.test.js
@@ -1,6 +1,7 @@
 // As a cruise ship captain,
-// So I can get passengers aboard a ship,
-// I want a ship to have a starting port.
+// So I can get passengers started on their journey,
+// I want a ship to be able to set sail from a port.
+const { it, expect } = require("@jest/globals");
 const Ship = require("./ship");
 describe("make a cruise ship that has passengers aboard, has a starting point and can accept methods", () => {
   let ship;
@@ -9,5 +10,12 @@ describe("make a cruise ship that has passengers aboard, has a starting point an
   });
   it("creates new object of ship", () => {
     expect(ship).toBeInstanceOf(Object);
+  });
+  it("cruise ship has a starting port", () => {
+    expect(ship.currentPort).toBe("liverpool");
+  });
+  it("test state change of ship", () => {
+    ship.setSail();
+    expect(ship.currentPort).toEqual("");
   });
 });
